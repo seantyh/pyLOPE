7219a7dda4af52cfc9cdd126edbd44690b4dd057
seantyh <seantyh@gmail.com>
2018-07-03 13:55:46 +0800

    initial commit

---
+++ pyLOPE/tests/__init__.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class DummyTest(TestCase):
+    def testDummy(self):
+        self.assertTrue(True)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pyLOPE',
+      version='0.1.0',
+      description='A python package for paedological purpose',
+      url='http://github.com/seantyh/pyLOPE',
+      author='Sean Tseng',
+      author_email='seantyh@gmail.com',
+      license='MIT',
+      install_requires=[
+      ],
+      packages=['pyLOPE'],
+      test_suite='pyLOPE.tests',
+      zip_safe=False)
+
